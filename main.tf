899fe4ac2c81f31461efbec1dad0f4cc8ef72584
Joás Garcia <joasdemelo@hotmail.com>
2024-03-18 22:40:32 -0300

    Utilizando arquivo ./aws/crendtials ao invés de deixar as credenciais hard coded.

--- main.tf
+++ main.tf
@@ -13,10 +13,9 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "ASIAVNIY2D7VSNFWLLOB"
-  secret_key = "88MLXrPCW2rCbJjvhEbtFhgI+PhMjOiRNQzgI4cf"
-  token = "FwoGZXIvYXdzEE4aDJMURPnqACpP9qxznyLFAc0adQyYtmZVNNMSfNff+pYDTokrtal/q0AkY0xu2L605Wz+CE3B6rwUj7Gtam66xbvjjbrwUdiW8qCkH1wQp8LhtPH7GCwC172tav+1xUTJ7KhEtd1/NQiROBKARkuQJwhAQRHZpFQLfNuprRD6LxoKpJ5Mv9fd+Hpl3oLtt+0iwe7BGLRZSGIWUwue88Ifhkb+D++WRqRHVbRxgvVPgMiaNZjwsiNtyTT+Gi3gTM47ssuUMbojEkinv7a+xm2LXYk80IuaKO+Q4K0GMi0JgYXW/OQzbpWZE51ahuRuLLxhVQCihbSL1/nwiTz8jwnV8bYzzjopP9ApOZU="
+  region  = "us-east-1"
+  profile = "lab"
+  shared_credentials_files = ["$HOME/.aws/credentials"]
 }
 
 resource "aws_dynamodb_table" "payment_table" {
